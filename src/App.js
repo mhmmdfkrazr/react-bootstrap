d1728ae78581b382b3f635c1c0af4e76a03e8ba5
mhmmdfkrazr <89122220+mhmmdfkrazr@users.noreply.github.com>
2022-01-04 16:19:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React from 'react';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function Header() {
+  return (
+    <>
+    <nav className="py-2 bg-light border-bottom">
+      <div className="container d-flex flex-wrap">
+        <ul className="nav me-auto">
+          <li className="nav-item">
+            <a href="/" className="nav-link link-dark px-2 activate" aria-current="page">Home</a>
+          </li>
+          <li className="nav-item"><a href="/" className="nav-link link-dark px-2">Features</a></li>
+          <li className="nav-item"><a href="/" className="nav-link link-dark px-2">FAQs</a></li>
+          <li className="nav-item"><a href="/" className="nav-link link-dark px-2">About</a></li>
+        </ul>
+        <ul className="nav">
+        <li className="nav-item"><a href="/" className="nav-link link-dark px-2">Login</a></li>
+        <li className="nav-item"><a href="/" className="nav-link link-dark px-2">Sign up</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <header className="py-3 mb-4 border-bottom">
+      <div className="container d-flex flex-wrap justify-content-center">
+        <a href="/" className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
+          <span className="fs-4">My First React App</span>
+        </a>
+        <form className="col-12 col-lg-auto mb-3 mb-lg-0">
+          <input type="search" className="form-control" placeholder="Search..." aria-label="Search" />
+        </form>
+      </div>
+    </header>
+    </>
+  )
+}
+
+function Content() {
+  return (
+    <>
+    <div className="p-5 mb-4 bg-light rounded-3 border">
+      <div className="container-fluid py-5">
+        <h1 className="display-5 fw-bold">Puisi</h1>
+      <p className="col-md-8 fs-4">
+        Aku ingin mencintaimu dengan sederhana,
+        dengan kata yang tak sempat diucapkan kayu kepada api yang menjadikannya abu.
+        <br />
+        <br />
+        Aku ingin mencintaimu dengan sederhana,
+        dengan isyarat yang tak sempat disampaikan awan kepada hujan yang menjadikannya tiada.
+      </p>
+      <button className="btn btn-primary btn-lg" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">Click here to show Modal box</button>
+      </div>
+    </div>
+
+    {/* Modal */}
+    <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">Modal Title</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div className="modal-body">
+            This is my first modal box
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" className="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+function Footer() {
+  return (
+    <footer className="text-muted py-5 border-top">
+      <div className="container">
+        <p className="mb-1">Muhammad Fikri Azhar</p>
+        <p className="mb-0">New to React? <a href="https://reactjs.org/">Visit the homepage</a></p>
+      </div>
+    </footer>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Header />
+    <div className="container">
+      <Content />
     </div>
+    <Footer />
+    </>
   );
 }
 
